ea0349f94c2a6645c935a10fadcbae947604615f
joyita-web <joyicareer@gmail.com>
2024-11-23 21:12:48 +0530

    Update providers.tf

--- projects/day3/providers.tf
+++ projects/day3/providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-#   access_key = "AKIAVPEYWA2EEQ3KEV52"
-#   secret_key = "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"
+#   access_key = ""
+#   secret_key = ""
 }
